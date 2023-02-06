c17be22ac34799fed4584b8905790b66bec1f799
Aziz <aziz.cse.cu@gmail.com>
2023-02-07 00:17:35 +0600

    Make UI layout for homepage with listview

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,22 +1,33 @@
 // ignore_for_file: avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/catalog.dart';
 
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-
+    final dummyList = List.generate(50, (index) => CatalogModel.item[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
       drawer: MyDrawer(),
-      body: Center(
-        child: Text("Hello This is Aziz"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
       ),
     );
   }
