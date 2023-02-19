e5efb4d9c786bb264503edbce6981eb60355cfb1
Aziz <aziz.cse.cu@gmail.com>
2023-02-19 22:31:11 +0600

    use gridView.builder instead Listview.Builder in home_page and do some styling

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -43,14 +43,50 @@ class _HomepageState extends State<Homepage> {
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.item == null)
             ? Center(
-              child: CircularProgressIndicator(),
-            )
-            : ListView.builder(
-                itemCount: CatalogModel.item?.length,
+                child: CircularProgressIndicator(),
+              )
+            : GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                ),
                 itemBuilder: (context, index) {
-                  return ItemWidget(item: CatalogModel.item![index]);
-                },
-              ),
+                  final item = CatalogModel.item![index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: GridTile(
+                      header: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                        ),
+                        child: Text(
+                          item.name,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      ),
+                      footer: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                        ),
+                        child: Text(
+                          item.price.toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )
+                      ),
+                      child: Image.network(item.image),
+                    ),
+                  );
+                }),
       ),
     );
   }
